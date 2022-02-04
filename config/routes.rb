156ec8c69429e412895ceeebc864a8488db91bab
su-kino <97873142+su-kino@users.noreply.github.com>
2022-02-04 08:52:00 +0000

    [Add]create,read,update機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  root :to => 'homes#top'
+  get 'books' => 'books#index', as: 'book'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
