c1a973a361733770eeb212603bbc9cbac9418df5
su-kino <97873142+su-kino@users.noreply.github.com>
2022-02-04 01:23:51 +0000

    [Add]必要ファイルの作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "4ed6d4368b99457483f37f44f71cd68a.vfs.cloud9.us-east-1.amazonaws.com"
 end
