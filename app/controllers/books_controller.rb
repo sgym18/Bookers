a6d31baaa7e62b8a7c4a0bc56f6cbc3b948cc143
su-kino <97873142+su-kino@users.noreply.github.com>
2022-02-05 01:08:40 +0000

    [Add]フラッシュメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to show_book_path(book.id)
   end
 
@@ -21,12 +22,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to show_book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to book_path
   end
 
