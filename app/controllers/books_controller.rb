4c170cda275681061b712f06e9ea403b7267ae94
su-kino <97873142+su-kino@users.noreply.github.com>
2022-02-05 00:46:23 +0000

    [Add]destroy機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,12 @@ class BooksController < ApplicationController
     redirect_to show_book_path(book.id)
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
